a55cb47ccb937f6e37e1cc76b6c944ab791e5707
DreamJain10 <dreampachori18@gmail.com>
2026-02-25 12:16:52 +0530

    open

---
+++ src/uc1.java
@@ -0,0 +1,13 @@
+public class uc1 {
+
+    public static void main(String[] args) {
+
+        String appName = "Palindrome Checker App";
+        String version = "Version 1.0";
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" " + version);
+        System.out.println("=================================");
+    }
+}
