d5fb8582b343a9a0f27854896fdb4ca06999be19
DreamJain10 <dreampachori18@gmail.com>
2026-03-12 09:49:37 +0530

    intialize

--- src/uc1.java
+++ src/uc1.java
@@ -1,13 +1,12 @@
-public class uc1 {
 
-    public static void main(String[] args) {
 
-        String appName = "Palindrome Checker App";
-        String version = "Version 1.0";
+    public class PalindromeCheckerApp {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to " + appName);
-        System.out.println(" " + version);
-        System.out.println("=================================");
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Palindrome Checker Application");
+            System.out.println("Application Name: Palindrome Checker");
+            System.out.println("Version: v1.0");
+
+        }
     }
-}
